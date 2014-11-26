c9a1b0112181ce10b606dee93881df5b4992ebb6
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-26 11:20:30 +0800

    Added a new parameter to check the returned POST request

--- lists/tests.py
+++ lists/tests.py
@@ -15,7 +15,9 @@ class HomepageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		expected_html = render_to_string('home.html')
+		expected_html = render_to_string('home.html',
+				{'new_item_text': 'A new list itemm'})
+
 		self.assertEqual(response.content.decode(),expected_html)
 
 	def test_home_page_can_save_a_POST_request(self):
