8e5f3b7e17543238c755518eb28931ac26f41a0c
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-26 11:01:43 +0800

    Added a unit test case to see if the request would return the expected item

--- lists/tests.py
+++ lists/tests.py
@@ -5,9 +5,9 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page
 
+
 # Create your tests here.
 class HomepageTest(TestCase):
-
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')	
 		self.assertEqual(found.func, home_page)
@@ -18,3 +18,11 @@ class HomepageTest(TestCase):
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(),expected_html)
 
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		
+		response = home_page(request)
+		self.assertIn('A new list item', response.content.decode())
+		
